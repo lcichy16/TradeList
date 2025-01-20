1d9c51dd5761b35ff558378344bf1c292b59b33e
lcichy16 <lukaszcic16@gmail.com>
2025-01-20 23:56:32 +0100

    Initial

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradeList.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength (100)]
+        public required string Name { get; set; }
+
+
+        [EmailAddress]
+        public required string Email { get; set; }
+
+
+        [MinLength(6)] 
+        public required string Password { get; set; }
+
+        
+    }
+}
